53bf97159f4a253890aaef555b70a0482f5dec05
David Durlan <david.a.durlan@gmail.com>
2019-12-31 10:46:24 +0100

    make answer input field toggle work

--- public/script.js
+++ public/script.js
@@ -2,11 +2,10 @@ const question = $('.main');
 
 question.on('click', function(e) {
     const item = $(e.target).next();
-    console.log(item);
 
     if (item.hasClass('highlight')) {
-        item.addClass('highlight');
-    } else {
         item.removeClass('highlight');
+    } else {
+        item.addClass('highlight');
     }
 });

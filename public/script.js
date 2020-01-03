01778ecd39cb600aa0e0625b978f483385fb3646
David Durlan <david.a.durlan@gmail.com>
2020-01-03 14:45:55 +0100

    add highlight to answers that aren't empty

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
-const question = $('.main');
+const question = $('.main'),
+    answers = $('.answer'),
+    answersInput = $('.answer textarea');
 
+// toggle highlight class on answer field
 question.on('click', function(e) {
     const item = $(e.target).next();
 
@@ -9,3 +12,18 @@ question.on('click', function(e) {
         item.addClass('highlight');
     }
 });
+
+//loop trough answer fields and add highlight class, if input isnt empty.
+function highlight() {
+    for (let i = 0; i < answersInput.length; i++) {
+        if (answersInput[i].value) {
+            var parent = answersInput
+                .eq(i)
+                .parent()
+                .parent();
+
+            parent.addClass('highlight');
+        }
+    }
+}
+highlight();

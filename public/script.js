7fa6c3e124f3cb46bc6da3c8f9d81462b5ed97da
David Durlan <david.a.durlan@gmail.com>
2020-01-04 22:30:01 +0100

    trying to make autosizing the textarea work, no sucess

--- public/script.js
+++ public/script.js
@@ -3,7 +3,19 @@ const question = $('.main'),
     answersInput = $('.answer textarea');
 
 //make textarea auto size themselves
-autosize($('textarea'));
+// autosize($('textarea'));
+// $('textarea').autoResize();
+$('textarea').on('input', function() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+    let lineCount = 0;
+    let lines = this.value;
+    let matches = lines.match(/\n/g);
+    let breaks = matches ? matches.length : 0;
+    console.log(lines);
+    console.log(matches);
+    console.log(breaks);
+});
 
 // toggle highlight class on answer field
 question.on('click', function(e) {
@@ -40,3 +52,13 @@ function highlight() {
     }
 }
 highlight();
+
+// make textarea submit on enter keypress
+$('.main textarea').keypress(function(e) {
+    if (e.which == 13 && !e.shiftKey) {
+        $(this)
+            .closest('form')
+            .submit();
+        e.preventDefault();
+    }
+});
